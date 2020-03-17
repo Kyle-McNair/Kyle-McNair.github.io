540ee0ad0988a5872c2004c7493e4bcad529ffde
Kyle-McNair <58085298+Kyle-McNair@users.noreply.github.com>
2020-03-16 22:41:34 -0500

    Tilemapping portfolio pieces

---
+++ js/NamingWI_TileMap.js
@@ -0,0 +1,11 @@
+var map = L.map('map', {
+    minZoom: -4,
+    maxZoom: 2,
+    zoom: 0.5,
+    crs: L.CRS.Simple,
+    Renderer: 10
+});
+var bounds = [[0,0], [11520,8640]];
+var image = L.imageOverlay('/images/NamingWI.png', bounds).addTo(map);
+
+map.fitBounds(bounds);
