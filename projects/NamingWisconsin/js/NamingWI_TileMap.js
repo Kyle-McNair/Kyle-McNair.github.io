378733dd82be213d5e1e474486aebe8b1ba6b33c
Kyle-McNair <58085298+Kyle-McNair@users.noreply.github.com>
2020-10-21 22:35:09 -0500

    Update NamingWI_TileMap.js

--- projects/NamingWisconsin/js/NamingWI_TileMap.js
+++ projects/NamingWisconsin/js/NamingWI_TileMap.js
@@ -6,6 +6,6 @@ var map = L.map('map', {
     Renderer: 10
 });
 var bounds = [[0,0], [11520,8640]];
-var image = L.imageOverlay('/Projects/NamingWisconsin/images/NamingWI.png', bounds).addTo(map);
+var image = L.imageOverlay('../NamingWisconsin/images/NamingWI.png', bounds).addTo(map);
 
 map.fitBounds(bounds);
